944d2b2d75701fc01cd0f157daad4beccd822300
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2025-02-19 07:57:25 +0100

    added step 1 and step 2

--- script.js
+++ script.js
@@ -14,26 +14,40 @@ const pbAgain = document.querySelector(".again");
 console.log(pbAgain);
 
 const pbGuess = document.querySelector(".check");
+const highScoreDoc = document.querySelector(".highscore");
+let highScore = highScoreDoc.innerHTML;
+console.log(highScore);
 
 let randomNumber = 0;
+let score = lblScore.innerHTML - 1;
 
 function generatedNumber() {
   randomNumber = Math.trunc(Math.random() * 20) + 1;
   lblNumber.textContent = randomNumber;
   console.log(randomNumber);
+  if (score > highScore) {
+    highScore = score;
+  }
 }
 
 pbAgain.addEventListener("click", generatedNumber);
 
 function guessNumber() {
-  const clientGuess = inpGuess.value;
+  let clientGuess = inpGuess.value;
+  let score = lblScore.innerHTML - 1;
   console.log(clientGuess);
 
   if (clientGuess == randomNumber) {
     lblMessage.textContent = "du har vundet";
-    console.log("vundet");
+    lblNumber.textContent = randomNumber;
   } else {
     lblMessage.textContent = "prøv igen";
+    if (score < 0) {
+      lblMessage.textContent =
+        "Du har prøvet for mange gange, din score er nu på 0";
+      score = 0;
+    } else lblScore.textContent = score;
+
     console.log("try again");
   }
 }
